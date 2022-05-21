7fa1db169b251d70f0da5c792ded20b16fae394a
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-21 15:04:57 -0300

    refactor: implements generateToken function

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,16 +1,10 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const generateToken = require('../utils/generateToken');
 const { OK_STATUS } = require('../utils/statusCode');
 
 module.exports = async (req, res) => {
   const { email } = req.body;
 
-  const jwtConfig = {
-    expiresIn: '1h',
-    algorithm: 'HS256',
-  };
-  
-  const token = jwt.sign({ data: { email } }, process.env.JWT_SECRET, jwtConfig);
+  const token = generateToken(email);
 
   res.status(OK_STATUS).json({ token });
 };
