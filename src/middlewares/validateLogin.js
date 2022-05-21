b9368fa62e21d5c1bd34451b93e7e3766303f3a4
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-21 18:15:32 -0300

    refactor: removes direct connection with database

--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -1,18 +1,11 @@
 const errorFunction = require('../utils/errorFunction');
 const { BAD_REQUEST } = require('../utils/statusCode');
-const { User } = require('../database/models');
 
 module.exports = async (req, _res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return next(errorFunction(BAD_REQUEST, 'Some required fields are missing'));
-  }
-
-  const user = await User.findOne({ where: { email } });
-
-  if (!user) {
-    return next(errorFunction(BAD_REQUEST, 'Invalid fields'));
   }
 
   return next();
