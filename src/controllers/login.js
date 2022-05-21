9284e0651dbaafe1bcc63eac5ea0da38c78603f6
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-21 18:18:58 -0300

    refactor: validation that requires database connection is now made through teh userService layer

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,8 +1,11 @@
 const generateToken = require('../utils/generateToken');
 const { OK_STATUS } = require('../utils/statusCode');
+const userService = require('../services/user');
 
 module.exports = async (req, res) => {
   const { email } = req.body;
+
+  await userService.getByEmail(email);
 
   const token = generateToken(email);
 
