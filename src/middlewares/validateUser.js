6052a42372d8a19397758b445cdd45587ef4e47b
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-21 15:07:01 -0300

    feat: adds validateUser middleware

---
+++ src/middlewares/validateUser.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+const errorFunction = require('../utils/errorFunction');
+
+const schema = Joi.object({
+  displayName: Joi.string().min(8).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  image: Joi.string(),
+});
+
+module.exports = (req, _res, next) => {
+  const { error } = schema.validate(req.body);
+
+  if (error) return next(errorFunction(400, error.message));
+
+  return next();
+};
