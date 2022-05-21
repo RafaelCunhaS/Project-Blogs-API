d0c93745cd98c03fb09f9adf0fec90a087485856
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-21 14:12:11 -0300

    fix: corrects the fuction parameters and implements next() on it

--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -2,18 +2,18 @@ const errorFunction = require('../utils/errorFunction');
 const { BAD_REQUEST } = require('../utils/statusCode');
 const { User } = require('../database/models');
 
-module.exports = async (body) => {
-  const { email, password } = body;
+module.exports = async (req, _res, next) => {
+  const { email, password } = req.body;
 
   if (!email || !password) {
-    throw errorFunction(BAD_REQUEST, 'Some required fields are missing');
+    return next(errorFunction(BAD_REQUEST, 'Some required fields are missing'));
   }
 
   const user = await User.findOne({ where: { email } });
 
   if (!user) {
-    throw errorFunction(BAD_REQUEST, 'Invalid fields');
+    return next(errorFunction(BAD_REQUEST, 'Invalid fields'));
   }
 
-  return true;
+  return next();
 };
