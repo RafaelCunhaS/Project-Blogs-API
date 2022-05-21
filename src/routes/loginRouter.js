fceabf8c38e78c4cc87b2a3be7797cc410b3cd28
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-21 18:56:09 -0300

    feat: adds import of rescue

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const rescue = require('express-rescue');
 const validateLogin = require('../middlewares/validateLogin');
 const loginController = require('../controllers/login');
 
-router.post('/', validateLogin, loginController);
+router.post('/', validateLogin, rescue(loginController));
 
 module.exports = router;
