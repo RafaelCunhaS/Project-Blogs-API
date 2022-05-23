292e3b250fac27bb8331e86086b2b4564f370b79
Rafael Santos <rafaelcs_94@hotmail.com>
2022-05-23 15:47:13 -0300

    refactor: refactoring data sent through token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-module.exports = (id, email) => {
+module.exports = (userId, email) => {
   const jwtConfig = {
     expiresIn: '1d',
     algorithm: 'HS256',
   };
   
-  const token = jwt.sign({ data: { id, email } }, process.env.JWT_SECRET, jwtConfig);
+  const token = jwt.sign({ data: { userId, email } }, process.env.JWT_SECRET, jwtConfig);
 
   return token;
 };
